b2dbf500a33621669fc8c9603dbd1857d0cfedcf
stefan19 <stefan.stefanescu@stud.ubbcluj.ro>
2023-04-16 22:34:25 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Parser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
